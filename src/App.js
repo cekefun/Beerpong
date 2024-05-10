9d66c13dbda710659b65547694b4285000b00901
Ceke <cekefun@gmail.com>
2024-05-10 19:58:23 +0200

    Update ip

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import FrontPage from './components/FrontPage';
 
 
 export default function Board() {
-  const websocket = new WebSocket("ws://localhost:9090");
+  const websocket = new WebSocket("ws://158.101.172.58:9090");
 
 
   return (
     <>
-    <img src='/images/CTTRedLogo.png' class="ct-logo"/>
-    <img src='/images/AL_transparentBOOST1.png' class="partner-logo" />
+    <img src='/images/CTTRedLogo.png' className="ct-logo"/>
+    <img src='/images/AL_transparentBOOST1.png' className="partner-logo" />
     <FrontPage websocket={websocket}/>
     <Rules/>
     
